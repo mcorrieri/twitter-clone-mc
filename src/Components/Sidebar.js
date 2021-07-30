422cc28fbe65968a6ea7ff276572399fe97a99a3
Michael <Mlcorrieri@gmail.com>
2021-07-30 00:27:51 -0400

    sidebar and styling finished

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -7,12 +7,13 @@ import ExploreIcon from "@material-ui/icons/Explore";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import MailIcon from "@material-ui/icons/Mail";
 import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
+import { Button } from "@material-ui/core/";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <TwitterIcon />
-      <SidebarOptions Icon={HomeIcon} text="Home" />
+      <SidebarOptions active Icon={HomeIcon} text="Home" />
       <SidebarOptions Icon={ExploreIcon} text="Explore" />
       <SidebarOptions Icon={NotificationsIcon} text="Notifications" />
       <SidebarOptions Icon={MailIcon} text="Messages" />
@@ -20,7 +21,9 @@ function Sidebar() {
       {/* Sidebar options  */}
       {/* Sidebar options  */}
 
-      {/* Tweet button  */}
+      <Button variant="outlined" fullWidth className="tweetbtn">
+        Tweet
+      </Button>
     </div>
   );
 }
--- src/Components/SidebarOptions.js
+++ src/Components/SidebarOptions.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./SidebarOptions.css";
 
-function SidebarOptions({ text, Icon }) {
+function SidebarOptions({ active, text, Icon }) {
   return (
-    <div className="sidebar-option">
+    <div className={`sidebar-option ${active && `sidebar-option--active`}`}>
       <Icon />
       <h2>{text}</h2>
     </div>
